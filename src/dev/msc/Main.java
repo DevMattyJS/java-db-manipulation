ade9d4018c516cadca057f9c2f9d76f14be54ff3
DevMattyJS <matej.sliacky@protonmail.com>
2023-09-23 17:44:52 +0200

    First commit - datasource class + artist, album, song classes are created. DB connection is also established and a method for query artists was added

---
+++ src/dev/msc/Main.java
@@ -0,0 +1,29 @@
+package dev.msc;
+
+import dev.msc.model.Artist;
+import dev.msc.model.Datasource;
+
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Datasource datasource = new Datasource();
+        if(!datasource.open()) {
+            System.out.println("Can't open datasource");
+            return;
+        }
+
+        List<Artist> artists = datasource.queryArtists();
+        if (artists == null) {
+            System.out.println("No artists!");
+            return;
+        }
+
+        for (Artist artist : artists) {
+            System.out.println("ID = " + artist.getId() + ", Name = " + artist.getName());
+        }
+
+        datasource.close();
+    }
+}
